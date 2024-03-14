595e5f0d125829445091da92cf05fba1ce4b4da0
Yaossg <yaossg@outlook.com>
2024-03-14 14:43:48 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
-make Porkchop -j8 &&
+make PorkchopLite -j$(nproc) &&
 clang -emit-llvm -S ../lib/lib.c -o lib.ll &&
-./Porkchop main.pc -o main.ll -l &&
+./PorkchopLite main.pc -o main.ll -l &&
 llvm-link -opaque-pointers main.ll lib.ll -S -o out.ll &&
 lli -opaque-pointers out.ll
 echo "returned with code" $?
